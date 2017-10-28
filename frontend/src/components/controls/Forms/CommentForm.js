517384c951e453232f66a4429c3c7a3ac6c1a1ff
AndrewPHunter <andrew.hunter@fiercetechinc.com>
2017-10-28 09:16:27 -0700

    Added comment form validation:

--- frontend/src/components/controls/Forms/CommentForm.js
+++ frontend/src/components/controls/Forms/CommentForm.js
@@ -6,7 +6,8 @@ import {Divider} from 'react-md/lib/Dividers';
 import {Button} from 'react-md/lib/Buttons';
 
 import {
-  generateTimeStamp
+  generateTimeStamp,
+  stringFormFieldRequired
 } from '../../../utilities/helpers';
 
 import './form.css';
@@ -32,11 +33,32 @@ class CommentForm extends Component{
     }
   };
 
+  static validation = {
+    author: (author)=>stringFormFieldRequired(author),
+    body: (body)=>stringFormFieldRequired(body)
+  };
+
   state = {
     comment:{
       ...CommentForm.defaultProps.comment,
       'parentId': this.props.parentId,
       ...this.props.comment
+    },
+    errors:{
+      author:false,
+      body:false
+    }
+  };
+
+  validateField = (field)=>{
+    const updatedField = Object.keys(field)[0];
+    const updatedValue = field[updatedField];
+    const isValid =
+      CommentForm.validation[updatedField] ?
+        CommentForm.validation[updatedField](updatedValue) : true;
+
+    return {
+      [updatedField] : !isValid
     }
   };
 
@@ -45,17 +67,56 @@ class CommentForm extends Component{
       ...this.state.comment,
       ...field
     };
-    this.setState({comment});
+
+    const errors = {
+      ...this.state.errors,
+      ...this.validateField(field)
+    };
+
+    this.setState({comment, errors});
+
+  };
+
+  isFormValid = ()=>{
+    const {errors}= this.state;
+    return(
+      Object.keys(errors)
+        .reduce((isValid, key)=>
+          isValid && !errors[key]
+        ,true)
+    );
+  };
+
+  validateForm = ()=>{
+    const {comment, errors} = this.state;
+    const errorState = Object
+      .keys(errors)
+      .reduce((formState, key)=>{
+        return {
+          ...formState,
+          ...this.validateField({[key]: comment[key]})
+        }
+      }, {});
+
+    return new Promise((resolve)=>{
+      this.setState({errors: errorState}, ()=>{
+        resolve(this.isFormValid());
+      });
+    });
   };
 
   render(){
-    const {comment} = this.state;
+    const {comment, errors} = this.state;
     const {onSave, onCancel} = this.props;
 
     return(
-      <form onSubmit={()=>{
+      <form onSubmit={async (e)=>{
+        e.preventDefault();
         comment.timestamp = generateTimeStamp();
-        onSave(comment);
+        const isValid = await this.validateForm();
+        if(isValid) {
+          onSave(comment);
+        }
       }}
       >
         <div className='md-grid'>
@@ -63,8 +124,10 @@ class CommentForm extends Component{
                      className='md-cell md-cell--6 md-cell--bottom'
                      label='Author'
                      placeholder='Author'
+                     error={errors.author}
                      value={comment.author}
                      onChange={(value) => this.onFieldUpdate({author: value})}
+                     errorText='Please provide an author'
           />
           <TextField id='date'
                      className='md-cell md-cell--6 md-cell--bottom'
@@ -72,17 +135,20 @@ class CommentForm extends Component{
                      disabled
                      value={new Date(comment.timestamp).toLocaleDateString()}
                      onChange={(value) => this.onFieldUpdate({timestamp: value})}
+                     errorText='This is for alignment only'
           />
           <TextField id='comment'
                      className='md-cell md-cell--12 md-cell--bottom'
                      placeholder='Comment'
                      rows={10}
                      value={comment.body}
+                     error={errors.body}
                      onChange={(value) => this.onFieldUpdate({body: value})}
+                     errorText='Please provide a comment'
           />
           <Divider/>
           <div className='md-cell md-cell--12 md-cell--bottom commands'>
-            <Button raised className='save' onClick={onSave.bind(null, this.state.comment)}>Save</Button>
+            <Button raised className='save' type='submit'>Save</Button>
             <Button raised className='cancel' onClick={onCancel}>Cancel</Button>
           </div>
         </div>
--- frontend/src/utilities/helpers.js
+++ frontend/src/utilities/helpers.js
@@ -3,3 +3,6 @@ import uuidv4 from 'uuid/v4';
 export const generateId = ()=>uuidv4();
 
 export const generateTimeStamp = ()=>Date.now();
+
+export const stringFormFieldRequired = (field)=>
+  field !== undefined && field !== null && field !== '';
