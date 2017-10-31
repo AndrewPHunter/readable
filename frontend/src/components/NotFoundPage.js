47be3ad29de1a0d4b9e4fb5e6d25dd9391fb0d30
AndrewPHunter <andrew.hunter@fiercetechinc.com>
2017-10-31 06:15:44 -0700

    Added 404 not found page

--- frontend/src/components/NotFoundPage.js
+++ frontend/src/components/NotFoundPage.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import {Paper} from 'react-md/lib/Papers';
+import {Media} from 'react-md/lib/Media';
+import {Button} from 'react-md/lib/Buttons'
+import {FontIcon} from 'react-md/lib/FontIcons'
+
+import notFound from './NotFoundPage.jpg';
+import './notFoundPage.css';
 
 const NotFoundPage = ()=>(
-  <div>
-    <h1>Post not found</h1>
-  </div>
+  <Paper className='notfound-page' zDepth={1}>
+    <div className='md-grid'>
+      <div className='md-cell md-cell--12'>
+        <h1>Post not found</h1>
+      </div>
+      <Paper className='md-cell md-cell--12 md-grid'>
+        <section className='md-cell md-cell--5'>
+          <Media>
+            <img src={notFound} alt='Post Not Found'/>
+          </Media>
+        </section>
+        <section className='md-cell md-cell--7 md-grid'>
+          <div className='md-cell md-cell--12'>
+            <h3>OOPS!</h3>
+            <p>
+              Sometimes despite our best intentions we all succumb to dysentery. But don't worry - it can happen
+              to the best of us - and it just happened to you.
+            </p>
+            <p>
+              Why don't you try again!
+            </p>
+          </div>
+          <div className='md-cell md-cell--12 link-home'>
+            <Button raised iconEl={<FontIcon>home</FontIcon>}>
+              <Link to='/' className=''>Home</Link>
+            </Button>
+          </div>
+        </section>
+      </Paper>
+    </div>
+  </Paper>
 );
 
 export default NotFoundPage;
